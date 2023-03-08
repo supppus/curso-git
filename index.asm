5b2c8fd4602855fc0870472a1cd548a04eab61c4
Victor Gomes <cartoonbr495@gmail.com>
2023-03-08 14:39:37 -0400

    Requisicao da senha

--- index.asm
+++ index.asm
@@ -2,6 +2,8 @@
 section .data ;
 	msg db 'sistema de login', 0xA, 0xD ; 
 	len equ $- msg ;
+	msg2 db 'digite sua senha', 0xA, 0xD ; 
+	len2 equ $- msg2 ;
 
 section .text ;
 
@@ -14,6 +16,12 @@ _start:
 	mov ecx, msg ; 
 	mov edx, len ;
 	int 0x80 ;
+
+	mov eax, 0x4 ; 
+	mov ebx, 0x1 ; 
+	mov ecx, msg2 ; 
+	mov edx, len2 ;
+	int 0x80 ; 
 
 	mov eax, 0x1 ; 
 	mov ebx, 0x0 ;
